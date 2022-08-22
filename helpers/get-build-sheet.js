66c54e052c6c0de58db1bbeb3d5d47ceceadb412
Martin Fracker, Jr <martinfrackerjr@gmail.com>
2022-08-21 23:13:11 -0500

    Add more vins to unit test

--- helpers/get-build-sheet.js
+++ helpers/get-build-sheet.js
@@ -3,7 +3,6 @@ const { fetchPdfContent } = require("./fetch-pdf-content");
 exports.getBuildSheet = async (vin) => {
   const buildSheetUrl = `https://www.jeep.com/webselfservice/BuildSheetServlet?vin=${vin}`;
   const content = await fetchPdfContent(buildSheetUrl);
-  console.log("contentLength", content.length);
   return {
     buildSheetFound: content.length > 5,
     buildSheetUrl,
--- helpers/get-build-sheet.test.js
+++ helpers/get-build-sheet.test.js
@@ -2,9 +2,20 @@ const { getBuildSheet } = require("./get-build-sheet");
 
 test("reports build sheet found", async () => {
   // test vins: https://www.jlwranglerforums.com/forum/threads/2023-build-sheets-are-being-detected-test-builds-showing-equipment-options-to-expect-in-2023-models.95500/
-  const { buildSheetFound } = await getBuildSheet("1C4HJXENXPW500039");
+  const promises = [
+    "1C4HJXDM2PW500044",
+    "1C4HJXFN5PW500061",
+    "1C4HJXEMXPW500081",
+    "1C4JJXSJ4PW500007",
+    "1C4JJXP60PW500025",
+    "1C4HJXENXPW500039",
+  ].map(async (vin) => {
+    const { buildSheetFound } = await getBuildSheet(vin);
 
-  expect(buildSheetFound).toBe(true);
+    expect(buildSheetFound).toBe(true);
+  });
+
+  await Promise.all(promises);
 });
 
 test("reports build sheet NOT found", async () => {
