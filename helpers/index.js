2b9ed437bc9a4c122eb3dbe636937cb9039e7368
Martin Fracker, Jr <martinfrackerjr@gmail.com>
2022-08-21 20:55:52 -0500

    Add build sheet to tracking email

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const { getTrackingInfo } = require("./get-tracking-info");
+const { getBuildSheet } = require("./get-build-sheet");
 
 exports.installHelpers = (app) => {
   Object.assign(app.context, {
     helpers: {
       getTrackingInfo,
+      getBuildSheet,
     },
   });
 };
--- routes/tracking-email/send-tracking-email.js
+++ routes/tracking-email/send-tracking-email.js
@@ -2,10 +2,18 @@ const axios = require("axios").default;
 
 exports.sendTrackingEmail = async (ctx) => {
   const { von, lastName } = ctx.params;
-  const locals = await ctx.helpers.getTrackingInfo(von, lastName);
+  const trackingInfo = await ctx.helpers.getTrackingInfo(von, lastName);
+  const { vin } = trackingInfo;
+  const { buildSheetFound, buildSheetUrl, extracted } =
+    await ctx.helpers.getBuildSheet(vin);
   await ctx.email.send({
     template: "jeep-tracking",
-    locals,
+    locals: {
+      ...trackingInfo,
+      buildSheetFound,
+      buildSheetFoundMsg: buildSheetFound ? "b.s. FOUND" : "b.s. NOT FOUND",
+      buildSheetUrl,
+    },
   });
 
   ctx.body = { success: true };
