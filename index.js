01570f7d9880204090eff0b96415dacc9a7a5d19
Martin Fracker, Jr <martinfrackerjr@gmail.com>
2022-08-21 19:28:23 -0500

    Add routes.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const Koa = require("koa");
 const app = new Koa();
-const Router = require("@koa/router");
 
-const axios = require("axios").default;
 const { installMailer } = require("./mailer");
+const { installRoutes } = require("./routes");
 
 const {
   MAILGUN_APIKEY,
@@ -22,43 +21,6 @@ installMailer(app, {
   to: EMAIL_TO,
 });
 
-const router = new Router();
-router.post("/tracking-email/:von/:lastName", async (ctx) => {
-  const { von, lastName } = ctx.params;
-  const { data } = await axios.get(
-    `https://www.jeep.com/hostz/cots/order-status/${von}/${lastName}`
-  );
-  const { orderstatus, vinDetails } = data;
-  const yStatuses = orderstatus.filter(
-    (status) => status.currentStatus === status.display
-  );
-  const { statusCode, statusDesc, statusUpdateDate } = yStatuses.pop();
-  const { brandName, modelYear, modelName, image, vin } = vinDetails;
-  const locals = {
-    statusCode,
-    statusDesc,
-    statusUpdateDate,
-    brandName,
-    modelYear,
-    modelName,
-    vehicle: `${modelYear} ${modelName}`,
-    image: `${image}&width=714&height=300&bkgnd=transparent&resp=png`,
-    vin,
-    von,
-  };
-  await ctx.email.send({
-    template: "jeep-tracking",
-    locals,
-  });
-
-  ctx.body = { success: true };
-});
-
-router.get("/", (ctx) => {
-  ctx.body = { path: "/" };
-});
-
-app.use(router.routes());
-app.use(router.allowedMethods());
+installRoutes(app);
 
 app.listen(PORT);
