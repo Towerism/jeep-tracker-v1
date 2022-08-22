4b442f73bdac4c677a0e4812e06abd68f4f1f6e0
Martin Fracker, Jr <martinfrackerjr@gmail.com>
2022-08-22 01:40:35 -0500

    Refactor sendTrackingEmail

--- routes/tracking-email/send-tracking-email.js
+++ routes/tracking-email/send-tracking-email.js
@@ -1,26 +1,35 @@
+const { promises } = require("nodemailer/lib/xoauth2");
+
 const axios = require("axios").default;
+
+async function aggregateTrackingInfo(von, lastName) {
+  const ctx = this;
+  const trackingInfo = await ctx.helpers.getTrackingInfo(von, lastName);
+  const { vin } = trackingInfo;
+
+  const [buildResponse, stickerResponse] = await Promise.all([
+    ctx.helpers.getBuildSheet(vin),
+    ctx.helpers.getWindowSticker(vin),
+  ]);
+  const { buildSheetFound, buildSheetUrl } = buildResponse;
+  const { windowStickerFound, windowStickerUrl } = stickerResponse;
+
+  return {
+    ...trackingInfo,
+    buildSheetFound,
+    buildSheetFoundMsg: buildSheetFound ? "b.s. 1" : "b.s. 0",
+    buildSheetUrl,
+    windowStickerFound,
+    windowStickerFoundMsg: windowStickerFound ? "w.s. 1" : "w.s. 0",
+    windowStickerUrl,
+  };
+}
 
 exports.sendTrackingEmail = async (ctx) => {
   const { von, lastName } = ctx.params;
-  const trackingInfo = await ctx.helpers.getTrackingInfo(von, lastName);
-  const { vin } = trackingInfo;
-  const { buildSheetFound, buildSheetUrl } = await ctx.helpers.getBuildSheet(
-    vin
-  );
-  const { windowStickerFound, windowStickerUrl } =
-    await ctx.helpers.getWindowSticker(vin);
-  await ctx.email.send({
-    template: "jeep-tracking",
-    locals: {
-      ...trackingInfo,
-      buildSheetFound,
-      buildSheetFoundMsg: buildSheetFound ? "b.s. 1" : "b.s. 0",
-      buildSheetUrl,
-      windowStickerFound,
-      windowStickerFoundMsg: windowStickerFound ? "w.s. 1" : "w.s. 0",
-      windowStickerUrl,
-    },
-  });
+  const locals = await aggregateTrackingInfo.bind(ctx)(von, lastName);
+
+  await ctx.email.send({ template: "jeep-tracking", locals });
 
   ctx.body = { success: true };
 };
